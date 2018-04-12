648a79df81156421110d2d9d85f3081f86e1ed12
Slobodan <slobodan.kosanovic@gmail.com>
2018-04-12 19:01:29 +0200

    a

--- src/com/kole/Animal.java
+++ src/com/kole/Animal.java
@@ -17,11 +17,11 @@ public class Animal {
     }
 
     public void eat(){
-
+        System.out.println("Animal.eat called!");
     }
 
-    public void move(){
-
+    public void move(int speed){
+        System.out.println("Animal is moving at " + speed);
     }
 
     public void setBody(int body) {
--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -3,6 +3,11 @@ package com.kole;
 public class Main {
 
     public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
+        Dog dog = new Dog("Yorkey", 8, 20, 2, 4, 1, 20, "Long silky");
+        dog.eat();
+        dog.walk();
+        dog.run();
     }
 }
